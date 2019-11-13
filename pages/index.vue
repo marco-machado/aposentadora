6f3deed826fb26bc3231bdbfff5528f73cb548b3
Marco Machado <marco.machado@gmail.com>
2019-11-12 21:13:07 -0300

    Commit inicial

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center">Aposentadora</div>
+      <v-card>
+        <v-card-title class="headline">Calculadora da aposentadoria</v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+        </v-card-text>
+        <v-card-actions></v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+};
+</script>
