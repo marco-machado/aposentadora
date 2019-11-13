0144fab931e26d7d19f841902ec3afea822bc439
Marco Machado <marco.machado@gmail.com>
2019-11-13 12:22:43 -0300

    Configuração do Google

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,12 @@ export default {
   */
   head: {
     titleTemplate: '%s @ Aposentadora',
-    title: 'Carregando',
+    title: 'Calculadora',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'google-site-verification', content: '4ij-5oWT6lwj4W5btShyxdfq7Ytk-xjukZzcPAwxbSk'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -35,13 +36,20 @@ export default {
   ** Nuxt.js dev-modules
   */
   buildModules: [
-    '@nuxtjs/vuetify',
+    '@nuxtjs/google-analytics',
+    '@nuxtjs/vuetify'
   ],
   /*
   ** Nuxt.js modules
   */
   modules: [
   ],
+  /**
+   * Google Analytics module configuration
+   */
+  googleAnalytics: {
+    id: 'UA-152344798-1'
+  },
   /*
   ** vuetify module configuration
   ** https://github.com/nuxt-community/vuetify-module
